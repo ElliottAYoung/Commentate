a20d074ef25a3ea404cd8f99c3b0257e74d1580b
Elliott Young <elliott.a.young@gmail.com>
2015-02-18 09:20:10 -0600

    Fix database seed bug

--- db/migrate/20150210073243_create_users.rb
+++ db/migrate/20150210073243_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :user_name, :password_hash, :profile_picture
+      t.string :user_name 
+      t.string :password_hash 
+      t.string :profile_picture
 
       t.timestamps
     end
