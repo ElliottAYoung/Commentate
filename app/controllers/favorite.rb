386192a8e9b4ae3d86ab9909ad2ed1d21d962d18
Elliott Young <elliott.a.young@gmail.com>
2015-02-18 15:22:26 -0600

    Fix favorites not adding...forgot to save them...awkward...

--- app/controllers/favorite.rb
+++ app/controllers/favorite.rb
@@ -1,7 +1,7 @@
 post '/users/:user_id/videos/:id' do
   redirect '/home' unless current_user
 
-  favorite = Favorite.new(user_id: params[:user_id], video_id: params[:id])
+  favorite = Favorite.create(user_id: params[:user_id], video_id: params[:id])
 
   redirect "/videos/#{params[:id]}"
 end
