37b69d4f8ccba36993729e48f50d6ec34f4d054d
Elliott Young <elliott.a.young@gmail.com>
2015-02-18 09:01:08 -0600

    Create Initial Tables and Seed data

--- db/migrate/20150218080352_create_videos.rb
+++ db/migrate/20150218080352_create_videos.rb
@@ -2,6 +2,7 @@ class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
       t.string     :video_name
+      t.string     :video_url
       t.text       :description
       t.integer    :rating
       t.belongs_to :user
