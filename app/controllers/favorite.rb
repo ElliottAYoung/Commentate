e4c2a6427de64d1634f90916ccf8cfd404887139
Elliott Young <elliott.a.young@gmail.com>
2015-02-18 14:09:07 -0600

    Add Favorite Button

--- app/controllers/favorite.rb
+++ app/controllers/favorite.rb
@@ -1,6 +1,14 @@
-get '/user/:id/favorites' do
+get '/users/:id/favorites' do
   @user = User.find(params[:id])
   @favorites = @user.favorites
 
   erb :"favorites/show"
+end
+
+post '/users/:user_id/videos/:id' do
+  redirect '/home' unless current_user
+
+  favorite = Favorite.new(user_id: params[:user_id], video_id: params[:id])
+
+  redirect "/videos/#{params[:id]}"
 end
