30665e705802e438a4beb3dff52ebdbbb4331f3a
Elliott Young <elliott.a.young@gmail.com>
2015-02-18 09:23:04 -0600

    Build video routes

--- app/controllers/video.rb
+++ app/controllers/video.rb
@@ -1,4 +1,35 @@
+# before '/videos/delete/:id' do
+#   # redirect '/home' unless 
+# end
+
 get '/home' do #RESTfully /videos
   @videos = Video.all
   erb :index
+end
+
+get '/videos/new' do
+  erb :"videos/new"
+end
+
+get '/video/:id' do
+  @video = Video.find(params[:id])
+
+  erb :"videos/show"
+end
+
+post '/videos' do
+  @video = Video.new(params[:video])
+
+  if @video.save
+    redirect '/home'
+  else
+    erb :"/videos/new"
+  end
+end
+
+delete '/videos/delete/:id' do
+  video = Video.find(params[:id])
+  video.delete
+
+  redirect '/home'
 end
